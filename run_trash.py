364a6b915f00c2aa4be12166eb40a5d38a9e9f97
Manning <dgmannin@ndc.nasa.gov>
2024-06-14 11:50:49 -0400

    oops

--- run_trash.py
+++
@@ -1,5 +0,0 @@
-import aviary.api as av
-from outputted_phase_info import phase_info
-prob = av.run_aviary('models/test_aircraft/Ces-500.csv',
-                     phase_info, optimizer="SLSQP", make_plots=True, max_iter=100)
-print(prob.get_val(av.Mission.Summary.FUEL_BURNED, units='lbm')[0])
